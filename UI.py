207eecf25db271ec1d6ccea26f9340cab70ec4c8
Joshua <19jelg@queensu.ca>
2023-04-12 15:44:32 -0400

    added new classifier

--- UI.py
+++ UI.py
@@ -1,9 +1,11 @@
 import tkinter
 import customtkinter
 import ajax_pipeline as ajax
+import pandas as pd
 
 from tkinter import *
 from tkinter import filedialog
+from tkinter.filedialog import asksaveasfile
 
 from matplotlib.figure import Figure
 from matplotlib.gridspec import GridSpec
@@ -11,28 +13,42 @@ from matplotlib.gridspec import GridSpec
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
 NavigationToolbar2Tk)
 
+from urllib import request
+
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("green")  # Themes: blue (default), dark-blue, green
 
 app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-app.geometry("840x480")
+app.geometry("1040x780")
 
-def button_function():
-    print("button pressed")
+# Create a sample DataFrame
+curr_dataframe = pd.DataFrame()
+
+def download_csv():
+    global curr_dataframe
+    #files = [('All Files', '*.*'), 
+    #         ('Python Files', '*.py'),
+    #         ('Text Document', '*.txt')]
+    #file = asksaveasfile(filetypes = files, defaultextension = files)
+    SAVING_PATH = filedialog.asksaveasfile(mode='w', defaultextension=".csv")
+    curr_dataframe.to_csv(SAVING_PATH)
+    print('CSV file downloaded successfully!')
 
 def browseFiles():
+    global curr_dataframe
     filename = filedialog.askopenfilename(initialdir = "/",
                                           title = "Select a File",
                                           filetypes = (("Text files",
                                                         "*.csv*"),
                                                        ("all files",
                                                         "*.*")))
-    button.configure(text=filename);
+    select_data_button.configure(text=filename);
     # the figure that will contain the plot
-    new_fig = ajax.evaluate_csv(filename)
+    new_fig, curr_dataframe = ajax.evaluate_csv(filename)
     canvas.figure = new_fig
+    print(curr_dataframe)
     canvas.draw()
-    new_fig.tight_layout()
+    #new_fig.tight_layout()
 
 frame = customtkinter.CTkFrame(master=app)
 frame.pack(pady=15,padx=60, fill="both", expand=True)
@@ -62,10 +78,14 @@ toolbar.update()
 # placing the toolbar on the Tkinter window
 #canvas.get_tk_widget().place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
+# Interactable Buttons
 currFileButtonText = "Select Acceleration Data"
-# Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=frame, text=currFileButtonText, command=browseFiles)
-button.pack(pady=12,padx=10)
+select_data_button = customtkinter.CTkButton(master=frame, text=currFileButtonText, command=browseFiles)
+
+download_data_button = customtkinter.CTkButton(master=frame, text="Download CSV", command=download_csv)
+
+download_data_button.pack(pady=12,padx=10)
+select_data_button.pack(pady=12,padx=10)
 
 footnote = customtkinter.CTkLabel(master=app, text="Developed by Jacintha Luo, Harrison Tigert and Joshua Gonzales.", font=("Roboto",12))
 footnote.pack(pady=12,padx=10)
--- ajax_pipeline.py
+++ ajax_pipeline.py
@@ -3,22 +3,26 @@ import pandas as pd
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
+import pickle
 
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import learning_curve, train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.metrics import roc_curve
 from sklearn.metrics import RocCurveDisplay
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
 #from sklearn.inspection import DecisionBoundaryDisplay
 from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 
 # Step 1: Read from csv files
 def read_csv_files(folder_path):
@@ -53,7 +57,7 @@ def read_csv_files(folder_path):
                 dataframe_collection[file_name]=pd.DataFrame(df)
     return dataframe_collection
 
-# Step 2: For each dataframe extract rolling window (and features?)
+# Step 2: For each dataframe extract rolling window and features
 
 # NOTE: Apparently we cannot store a rolling window object into hdf5 BUT we can store the "features" of the rolling window instead into that file
 # TO DO: Feature extraction guy, make it so we add each rolling window feature into rolling_window_collection
@@ -69,41 +73,164 @@ def extract_rolling_window(data_dict):
     """
     rolling_window_collection = {}
     for key in data_dict.keys():
-        features = pd.DataFrame(columns = ['max', 'min', 'mean', 'median', 'range', 'std', 'var', 'kurt', 'skew', 'state'])
+        features = pd.DataFrame(columns = [
+            'x_max', 'x_min', 'x_mean', 'x_median', 'x_range', 'x_std', 'x_var', 'x_kurt', 'x_skew',
+            'y_max', 'y_min', 'y_mean', 'y_median', 'y_range', 'y_std', 'y_var', 'y_kurt', 'y_skew',
+            'z_max', 'z_min', 'z_mean', 'z_median', 'z_range', 'z_std', 'z_var', 'z_kurt', 'z_skew', 'state'])
         print("extracting " + key)
         df = data_dict[key]
         df = df.astype('float64')
         df_preprocessed = preprocess_dataframe(df)
-        df_abs = df_preprocessed.iloc[:,4]
-        max = df_abs.rolling(window=5).max()                 
-        min = df_abs.rolling(window=5).min()
-        mean = df_abs.rolling(window=5).mean()
-        median =df_abs.rolling(window=5).median()
-        range = df_abs.rolling(window=5).apply(lambda x: x.max() - x.min())
-        std = df_abs.rolling(window=5).std()
-        var = df_abs.rolling(window=5).var()
-        kurt = df_abs.rolling(window=5).kurt()
-        skew = df_abs.rolling(window=5).skew()
-        state = df.iloc[:,5].rolling(window=5).mean() # idk even know what im doing here lmao
+
+        df_dim = df_preprocessed.iloc[:,1]
+        max = df_dim.rolling(window= 500).max()                 
+        min = df_dim.rolling(window= 500).min()
+        mean = df_dim.rolling(window= 500).mean()
+        median =df_dim.rolling(window= 500).median()
+        range = df_dim.rolling(window= 500).apply(lambda x: x.max() - x.min())
+        std = df_dim.rolling(window= 500).std()
+        var = df_dim.rolling(window= 500).var()
+        kurt = df_dim.rolling(window= 500).kurt()
+        skew = df_dim.rolling(window= 500).skew()
         #print(max)
-        features['max'] = max                
-        features['min'] = min
-        features['mean'] = mean
-        features['median'] = median
-        features['range'] = range
-        features['std'] = std
-        features['var'] = var
-        features['kurt']= kurt
-        features['skew'] = skew
+        features['x_max'] = max                
+        features['x_min'] = min
+        features['x_mean'] = mean
+        features['x_median'] = median
+        features['x_range'] = range
+        features['x_std'] = std
+        features['x_var'] = var
+        features['x_kurt']= kurt
+        features['x_skew'] = skew
+
+        df_dim = df_preprocessed.iloc[:,2]
+        max = df_dim.rolling(window= 500).max()                 
+        min = df_dim.rolling(window= 500).min()
+        mean = df_dim.rolling(window= 500).mean()
+        median =df_dim.rolling(window= 500).median()
+        range = df_dim.rolling(window= 500).apply(lambda x: x.max() - x.min())
+        std = df_dim.rolling(window= 500).std()
+        var = df_dim.rolling(window= 500).var()
+        kurt = df_dim.rolling(window= 500).kurt()
+        skew = df_dim.rolling(window= 500).skew()
+        #print(max)
+        features['y_max'] = max                
+        features['y_min'] = min
+        features['y_mean'] = mean
+        features['y_median'] = median
+        features['y_range'] = range
+        features['y_std'] = std
+        features['y_var'] = var
+        features['y_kurt']= kurt
+        features['y_skew'] = skew
+
+        df_dim = df_preprocessed.iloc[:,3]
+        max = df_dim.rolling(window= 500).max()                 
+        min = df_dim.rolling(window= 500).min()
+        mean = df_dim.rolling(window= 500).mean()
+        median =df_dim.rolling(window= 500).median()
+        range = df_dim.rolling(window= 500).apply(lambda x: x.max() - x.min())
+        std = df_dim.rolling(window= 500).std()
+        var = df_dim.rolling(window= 500).var()
+        kurt = df_dim.rolling(window= 500).kurt()
+        skew = df_dim.rolling(window= 500).skew()
+        #print(max)
+        features['z_max'] = max                
+        features['z_min'] = min
+        features['z_mean'] = mean
+        features['z_median'] = median
+        features['z_range'] = range
+        features['z_std'] = std
+        features['z_var'] = var
+        features['z_kurt']= kurt
+        features['z_skew'] = skew
+
+        state = df.iloc[:,5].rolling(window= 500).mean() # idk even know what im doing here lmao
         features['state'] = state # idk even know what im doing here lmao
 
         rolling_window_collection[key + "_features"] = features
 
     return rolling_window_collection
 
+# Helper method, used to get features without a specific label
+def get_features_from_csv(df):
+    features = pd.DataFrame(columns = ['x_max', 'x_min', 'x_mean', 'x_median', 'x_range', 'x_std', 'x_var', 'x_kurt', 'x_skew',
+            'y_max', 'y_min', 'y_mean', 'y_median', 'y_range', 'y_std', 'y_var', 'y_kurt', 'y_skew',
+            'z_max', 'z_min', 'z_mean', 'z_median', 'z_range', 'z_std', 'z_var', 'z_kurt', 'z_skew'])
+    
+    df = df.astype('float64')
+    df_preprocessed = preprocess_dataframe(df)
+    df_dim = df_preprocessed.iloc[:,1]
+    max = df_dim.rolling(window= 500).max()                 
+    min = df_dim.rolling(window= 500).min()
+    mean = df_dim.rolling(window= 500).mean()
+    median =df_dim.rolling(window= 500).median()
+    range = df_dim.rolling(window= 500).apply(lambda x: x.max() - x.min())
+    std = df_dim.rolling(window= 500).std()
+    var = df_dim.rolling(window= 500).var()
+    kurt = df_dim.rolling(window= 500).kurt()
+    skew = df_dim.rolling(window= 500).skew()
+    #print(max)
+    features['x_max'] = max                
+    features['x_min'] = min
+    features['x_mean'] = mean
+    features['x_median'] = median
+    features['x_range'] = range
+    features['x_std'] = std
+    features['x_var'] = var
+    features['x_kurt']= kurt
+    features['x_skew'] = skew
+
+    df_dim = df_preprocessed.iloc[:,2]
+    max = df_dim.rolling(window= 500).max()                 
+    min = df_dim.rolling(window= 500).min()
+    mean = df_dim.rolling(window= 500).mean()
+    median =df_dim.rolling(window= 500).median()
+    range = df_dim.rolling(window= 500).apply(lambda x: x.max() - x.min())
+    std = df_dim.rolling(window= 500).std()
+    var = df_dim.rolling(window= 500).var()
+    kurt = df_dim.rolling(window= 500).kurt()
+    skew = df_dim.rolling(window= 500).skew()
+    #print(max)
+    features['y_max'] = max                
+    features['y_min'] = min
+    features['y_mean'] = mean
+    features['y_median'] = median
+    features['y_range'] = range
+    features['y_std'] = std
+    features['y_var'] = var
+    features['y_kurt']= kurt
+    features['y_skew'] = skew
+
+    df_dim = df_preprocessed.iloc[:,3]
+    max = df_dim.rolling(window= 500).max()                 
+    min = df_dim.rolling(window= 500).min()
+    mean = df_dim.rolling(window= 500).mean()
+    median =df_dim.rolling(window= 500).median()
+    range = df_dim.rolling(window= 500).apply(lambda x: x.max() - x.min())
+    std = df_dim.rolling(window= 500).std()
+    var = df_dim.rolling(window= 500).var()
+    kurt = df_dim.rolling(window= 500).kurt()
+    skew = df_dim.rolling(window= 500).skew()
+    #print(max)
+    features['z_max'] = max                
+    features['z_min'] = min
+    features['z_mean'] = mean
+    features['z_median'] = median
+    features['z_range'] = range
+    features['z_std'] = std
+    features['z_var'] = var
+    features['z_kurt']= kurt
+    features['z_skew'] = skew
+    return features
+
 # Step 2.1 : Preprocess each dataset before extracting features
 def preprocess_dataframe(df):
     print("Preprocessing...")
+    window_size = 51
+    alpha = 0.1
+    #df = df.rolling(window_size).mean()
+    df = df.ewm(alpha, min_periods=window_size).mean()
     sc = preprocessing.StandardScaler()
     df = pd.DataFrame(data=sc.fit_transform(df))
     return df
@@ -138,20 +265,141 @@ def write_windows_to_hdf5_group(dataframes_dict, file_path, group):
 def classifier(df):
     df = df.dropna()
     print("Splitting dataframe to test and train")
-    X_train, X_test, Y_train, Y_test = train_test_split(df[['max', 'min', 'mean', 'median', 'range', 'std', 'var', 'kurt', 'skew']], df['state'], test_size=0.1, shuffle=True)
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        df[['x_max', 'x_min', 'x_mean', 'x_median', 'x_range', 'x_std', 'x_var', 'x_kurt', 'x_skew',
+            'y_max', 'y_min', 'y_mean', 'y_median', 'y_range', 'y_std', 'y_var', 'y_kurt', 'y_skew',
+            'z_max', 'z_min', 'z_mean', 'z_median', 'z_range', 'z_std', 'z_var', 'z_kurt', 'z_skew']], 
+        df['state'], test_size=0.1, shuffle=True)
     # create the classifier
+    rf = RandomForestClassifier(n_estimators=100, max_depth=5)
     l_reg = LogisticRegression(max_iter = 10000)
     scaler = StandardScaler()
-    clf = make_pipeline(scaler,l_reg)
-    #i need to do this
+
+    clf1 = make_pipeline(scaler,l_reg)
+    clf2 = make_pipeline(scaler,rf)
+
+    clf = VotingClassifier(estimators=[('lr', clf1), ('rf', clf2)], voting='soft')
+    
     print("fitting to pipeline")
     clf.fit(X_train, Y_train)
-    print("\n### COMPLETE ###\n")
-    accuracy = clf.score(X_test,Y_test)
-    print('Accuracy:', accuracy)
+    print("\n### FITTING COMPLETE ###\n")
+    # Define the training curve parameters
+    #train_sizes = np.linspace(0.1, 1.0, 10)  # Training set sizes as a fraction of total dataset size
+    #train_sizes_abs, train_scores, test_scores = learning_curve(
+    #    clf, X_train, Y_train, train_sizes=train_sizes, cv=5, scoring='neg_mean_squared_error')
+#
+    ## Compute the mean and standard deviation of training and test scores
+    #train_scores_mean = -np.mean(train_scores, axis=1)
+    #train_scores_std = np.std(train_scores, axis=1)
+    #test_scores_mean = -np.mean(test_scores, axis=1)
+    #test_scores_std = np.std(test_scores, axis=1)
+#
+    ## Plot the training curve
+    #plt.figure(figsize=(10, 6))
+    #plt.title("Training Curve")
+    #plt.xlabel("Training Set Size")
+    #plt.ylabel("Mean Squared Error")
+    #plt.grid()
+    #plt.fill_between(train_sizes_abs, train_scores_mean - train_scores_std,
+    #                 train_scores_mean + train_scores_std, alpha=0.1, color="r")
+    #plt.fill_between(train_sizes_abs, test_scores_mean - test_scores_std,
+    #                 test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    #plt.plot(train_sizes_abs, train_scores_mean, 'o-', color="r", label="Training score")
+    #plt.plot(train_sizes_abs, test_scores_mean, 'o-', color="g", label="Cross-validation score")
+    #plt.legend(loc="best")
+    #plt.show()
+    
+    print("\n### TRAINING COMPLETE ###\n")
+
+
+    # --------- Classifier Metrics ------- #
+    y_pred_clf = clf.predict(X_test)
+    acc = accuracy_score(Y_test, y_pred_clf)
+    recall = recall_score(Y_test, y_pred_clf)
+    f1 = f1_score(Y_test, y_pred_clf)
+    precision = precision_score(Y_test, y_pred_clf)
+    roc_auc = roc_auc_score(Y_test, y_pred_clf)
+    confusion = confusion_matrix(Y_test, y_pred_clf)
+    classification = classification_report(Y_test, y_pred_clf)
+    #accuracy = clf.score(X_test,Y_test)
+    print('#### Classifier Metrics ####')
+    print('Accuracy:', acc)
+    print('Recall:', recall)
+    print('F1 Score:', f1)
+    print("Precision:", precision)
+    print("ROC AUC:", roc_auc)
+    print("Confusion Matrix:\n", confusion)
+    print("Classification Report:\n", classification)
+    # -------------------------------------#
+
+    #evaluate_csv('C:\\Users\\joshu\Project-Ajax\\Evaluation Dataset\\sample_jumping.csv')
+
+    return clf
+
+def evaluate_csv(file_path):
+    current_dir = os.getcwd()
+    filename = os.path.join(current_dir, 'finalized_model.sav')
+    clf = pickle.load(open(filename, 'rb'))
+    testing_dataframe = pd.read_csv(file_path)
+
+    testing_dataframe_features = get_features_from_csv(testing_dataframe)
+    testing_dataframe_features = testing_dataframe_features.fillna(0)
+    print(testing_dataframe_features)
+    y_pred = clf.predict(testing_dataframe_features)
+    print(y_pred)
+    y_pred_df = pd.DataFrame({'state': y_pred})
+
+    result_dataframe = pd.concat([testing_dataframe_features, y_pred_df], axis=1)
+
+    num_walk_flag = np.count_nonzero(y_pred == 1)
+    num_run_flag = np.count_nonzero(y_pred == 2)
+    ratio_walk = num_walk_flag / len(y_pred)
+    print("Walk confidence = ", ratio_walk)
+
+    fig, ax = plt.subplots(figsize = (8.5, 4),
+             dpi = 100)
+    x = testing_dataframe['Time (s)']
+    y = testing_dataframe['Absolute acceleration (m/s^2)']
+    
+    ax.plot(x,y)
+
+    # Find the indices where y_pred is 2
+    y_pred_2_indices = np.where(y_pred == 2)[0]
+
+    # Find the indices where y_pred is 1
+    y_pred_1_indices = np.where(y_pred == 1)[0]
+
+    # Create a mask for y_pred = 2
+    mask_2 = np.zeros_like(y, dtype=bool)
+    mask_2[y_pred_2_indices] = True
+
+    # Create a mask for y_pred = 1
+    mask_1 = np.zeros_like(y, dtype=bool)
+    mask_1[y_pred_1_indices] = True
+
+    # Create a mask to exclude the first 5sec points (due to rolling window)
+    mask_exclude = np.zeros_like(y, dtype=bool)
+    mask_exclude[500:] = True
+    print(mask_exclude)
+
+    mask_1 = np.logical_and(mask_exclude, mask_1)
+    mask_2 = np.logical_and(mask_exclude, mask_2)
+
+    # Shade the areas where y_pred is 2 as green
+    jumping_section = plt.fill_between(x, y, where=mask_2, color='green', alpha=0.5)
+
+    # Shade the areas where y_pred is 1 as grey
+    walking_section = plt.fill_between(x, y, where=mask_1, color='grey', alpha=0.5)
+
+    ax.legend([walking_section, jumping_section], ['Walking', 'Jumping'])
+    ax.set_xlabel('Time Elapsed(s)')
+    ax.set_ylabel('Absolute Acceleration(m/s^2)')
+
+    #plt.show()
+    return fig, result_dataframe
 
 # Main Function
-def main(): 
+def init_pipeline(): 
     print("starting...")
 
     current_dir = os.getcwd()
@@ -183,6 +431,7 @@ def main():
     dataMem3 = {}
 
     # dictionary containing data frames of raw data
+    # original
     dataMem1 = read_csv_files(folder_paths[0])
     dataMem2 = read_csv_files(folder_paths[1])
     dataMem3 = read_csv_files(folder_paths[2])
@@ -199,22 +448,30 @@ def main():
             feature_list.append(df)
 
     feature_dataframe = pd.concat(feature_list,ignore_index=True)
-    #print(feature_dataframe)
+    print(feature_dataframe)
     # TO DO: Add dictionary for windows of data frames
     print("Initiating classification")
-    classifier(feature_dataframe)
+    clf = classifier(feature_dataframe)
+
+    filename = 'finalized_model.sav'
+    file = open(filename, 'wb')
+    pickle.dump(clf, file)
+    file.close()
+
     output_file = 'data.h5'
     output = h5py.File(output_file, 'w')
     mem1_group = output.create_group("Harrison Dataset")
     mem2_group = output.create_group("Josh Dataset")
     mem3_group = output.create_group("Jacintha Dataset")
-    classifier_data = output.create_group("Classifier Data")
+    classifier_data_group = output.create_group("Classifier Data")
 
     write_dataframes_to_hdf5_group(dataMem1, output_file, mem1_group)
     write_dataframes_to_hdf5_group(dataMem2, output_file, mem2_group)
     write_dataframes_to_hdf5_group(dataMem3, output_file, mem3_group)
-
-    # TO DO: add classifier data to a group
+    #classifier_data_group.create_dataset('classifier', data=clf)
+    # save the model to disk
+    
+    evaluate_csv('C:\\Users\\joshu\Project-Ajax\\Evaluation Dataset\\sample_jumping.csv')
 
     # Prints HDF5 structure
     #with h5py.File(output_file, 'r') as hdf:
@@ -239,6 +496,6 @@ def main():
     #        print(item)
 
 if __name__ == "__main__":
-    main()
-
+    init_pipeline()
+    #evaluate_csv('C:\\Users\\joshu\Project-Ajax\\Evaluation Dataset\\sample_jumping.csv')
     #Harrison
